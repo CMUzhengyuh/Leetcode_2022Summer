74f4cea6c481230e9eb2bd638d434655190374f5
Hobo965859229 <90294050+CMUzhengyuh@users.noreply.github.com>
2022-05-22 22:35:04 -0400

    05/22

---
+++ LinkedList/src/Leetcode234.java
@@ -0,0 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+/**
+ * 05/22/2022
+ */
+public class Leetcode234 {
+    /**
+     * Leetcode 234 - Palindrome Linked List (Easy)
+     * TC: O(n)
+     * SC: O(n)
+     * Fast pointer moves 2 units at a time until the end
+     * Slow pointer moves 1 unit at a time and push value to stack.
+     * Adjust the slow pointer to the start point of right part.
+     * Check the equality of corresponding elements.
+     */
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        Deque<Integer> stack = new ArrayDeque<>();
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            stack.push(slow.val);
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        while (slow != null) {
+            if (slow.val != stack.pop()) {
+                return false;
+            }
+            slow = slow.next;
+        }
+        return true;
+    }
+}
